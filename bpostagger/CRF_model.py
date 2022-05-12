ea5211e197dd85c14dd12f043672a10f8592e2c4
mika <mikaellajarapa@gmail.com>
2022-05-12 12:41:24 +0800

    remove engish checker and add error hadling for validating corpus

--- bpostagger/CRF_model.py
+++ bpostagger/CRF_model.py
@@ -147,10 +147,6 @@ englishChecker = enchant.Dict("en_US")
 def inputTextChecker(input):
     user_input = input.split(" ")
     sentenceLength = len(user_input)
-    for word in user_input:
-        if englishChecker.check(word) == True:
-            print('There are english words in the inputs')
-            return True
     special_char = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
     # check if input is a one word
     if sentenceLength == 1:
@@ -191,20 +187,29 @@ def saveTaggedInputTextToFile(taggedText):
     return taggedText
 
 def mergeTextFiles():
-    file_to_read =r"bpostagger\datasets\user_inputs.txt"
-    write_to_file=r"bpostagger\datasets\tagged_validated.txt"
+    try:
+        file_to_read =r"bpostagger\datasets\user_inputs.txt"
+        write_to_file=r"bpostagger\datasets\tagged_validated.txt"
 
-    # Reading a file
-    file = open(file_to_read,"r")
-    data = file.read()
-    file.close()
+        # Reading a file
+        file = open(file_to_read,"r")
+        data = file.read()
+        file.close()
 
-    # Writing to a file
-    with open(write_to_file,"a") as file:   # with method auto closes the file object
         if os.stat(file.name).st_size != 0:
-            file.write("\n")
-        file.write(data)
-    print('Completed')
+            # Writing to a file
+            with open(write_to_file,"a") as file:   # with method auto closes the file object
+                if os.stat(file.name).st_size != 0:
+                    file.write("\n")
+                file.write(data)
+            print('Completed')
+            # removing texts in user inputs text file after validation
+            f = open(file_to_read, 'r+')
+            f.truncate(0)
+        else:
+            print('Error no content')
+    except:
+        print('No Content')
 
 def textFileTransfer(updated_input, file):
     # Separating sentences if user inputs multiple sentences
